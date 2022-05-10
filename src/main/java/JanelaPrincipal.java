5ce40dfccca60f44216b39519df51ca4f7778777
Belisa Lopes <2200724@my.ipleiria.pt>
2022-05-10 22:42:10 +0100

    janelaPrincipal

---
+++ src/main/java/JanelaPrincipal.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class JanelaPrincipal {
+    private JButton veiculosButton;
+    private JButton oficinaButton;
+    private JButton eventosButton;
+    private JButton transaçõesButton;
+    private JButton clientesButton;
+    private JButton estatísticasButton;
+}
