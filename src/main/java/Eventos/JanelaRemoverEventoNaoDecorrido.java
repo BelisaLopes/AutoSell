27fae20ec78a2a46cfd1b2b7a1cd809e043df431
Belisa Lopes <2200724@my.ipleiria.pt>
2022-05-15 18:41:01 +0100

    Janela eventos done

---
+++ src/main/java/Eventos/JanelaRemoverEventoNaoDecorrido.java
@@ -0,0 +1,13 @@
+package Eventos;
+
+import javax.swing.*;
+
+public class JanelaRemoverEventoNaoDecorrido {
+    private JButton estatisticasButton;
+    private JButton veiculosButton;
+    private JButton clientesButton;
+    private JButton transaçõesButton;
+    private JButton oficinaButton;
+    private JButton eventosButton;
+    private JButton removerEventoButton;
+}
---
+++ src/main/java/Eventos/JanelaTransferirVeiculos.java
@@ -0,0 +1,13 @@
+package Eventos;
+
+import javax.swing.*;
+
+public class JanelaTransferirVeiculos {
+    private JButton estatisticasButton;
+    private JButton veiculosButton;
+    private JButton clientesButton;
+    private JButton transaçõesButton;
+    private JButton eventosButton;
+    private JButton oficinaButton;
+    private JButton transferirButton;
+}
