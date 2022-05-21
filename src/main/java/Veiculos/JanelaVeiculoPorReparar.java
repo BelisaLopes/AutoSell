b5cbb39f9f0e9790afdbf9a20402c836b2eeeecb
André Pinto <andresantospinto26@gmail.com>
2022-05-21 21:46:52 +0100

    veiculoPorReparar

---
+++ src/main/java/Veiculos/JanelaVeiculoPorReparar.java
@@ -0,0 +1,19 @@
+package Veiculos;
+
+import javax.swing.*;
+
+public class JanelaVeiculoPorReparar {
+    private JButton veículosButton;
+    private JButton estatísticasButton;
+    private JButton clientesButton;
+    private JButton eventosButton;
+    private JButton oficinaButton;
+    private JButton transaçõesButton;
+    private JComboBox comboBox1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField5;
+    private JList list2;
+    private JButton apresentarVeículosButton;
+    private JButton definirVeículoPorRepararButton;
+}
